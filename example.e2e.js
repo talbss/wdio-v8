311b7306fe597c94fc112e4a8661b0f7c2ecd78f
Boutaleb <boutaleb.boukharchouf@getyooz.com>
2024-04-17 16:31:25 +0200

    update test case mocha timeout

--- example.e2e.js
+++ example.e2e.js
@@ -1,14 +1,21 @@
-describe('My Login application', () => {
-    it('should login with valid credentials', async () => {
-        await browser.url(`https://the-internet.herokuapp.com/login`)
+describe('My mocha timeout tests - global overwrite', function () {
+    this.timeout(6000);
 
-        await $('#username').setValue('tomsmith')
-        await $('#password').setValue('SuperSecretPassword!')
-        await $('button[type="submit"]').click()
-
-        await expect($('#flash')).toBeExisting()
-        await expect($('#flash')).toHaveTextContaining(
-            'You logged into a secure area!')
+    it('should not timeout: pause > 3000 and < 6000', async function() {
+        console.log('test timeout: ' + this.test._timeout);
+        await browser.pause(5000);
+        console.log('test timeout: ' + this.test._timeout);
     })
+
 })
 
+describe('My mocha timeout tests - per it overwrite', function () {
+
+    it('should not timeout: pause > 3000 and < 6000', async function() {
+        console.log('test timeout before overwrite: ' + this.test._timeout);
+        this.timeout(6000);
+        console.log('test timeout after overwrite: ' + this.test._timeout);
+        await browser.pause(5000);
+    })
+
+})
--- wdio.conf.js
+++ wdio.conf.js
@@ -1,6 +1,3 @@
-const myCustomeReporter = require('./myCustomeReporter').default;
-
-
 exports.config = {
     //
     // ====================
@@ -115,7 +112,6 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services: ['selenium-standalone'],
     
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
@@ -137,14 +133,14 @@ exports.config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter
-    reporters: ['spec', myCustomeReporter],
+    reporters: ['spec'],
     
     //
     // Options to be passed to Mocha.
     // See the full list at http://mochajs.org/
     mochaOpts: {
         ui: 'bdd',
-        timeout: 60000
+        timeout: 3000
     },
     //
     // =====
